cc9239c66830d944fb9d58d7600da9cccda0169f
DinosDev <erick@bilhalba.com.br>
2022-05-20 15:35:52 -0300

    feat(RC 01-02): Create Index Route for Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,13 @@ class PostController extends Controller
 {
     public function index(Request $Request)
     {
+        $HiddenValues = ['content', 'created_at', 'updated_at'];
+
+        $Posts = Post::all()->makeHidden($HiddenValues);
+
+        $PostsArray = $Posts->toArray();
+
+        return $PostsArray;
     }
 
     public function show(Request $Request)
