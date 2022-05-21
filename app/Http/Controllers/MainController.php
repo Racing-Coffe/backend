7994b1932c9694b76f5de4360a5a51f7e13e7c64
DinosDev <erick@bilhalba.com.br>
2022-05-21 14:42:00 -0300

    feat(RC-04 00): Create MainController

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+abstract class MainController extends Controller
+{
+    protected function ValidateId(Request $Request)
+    {
+        $Request->merge(['id' => $Request->id]);
+        $Validation = ['id' => 'required|integer'];
+        $Validator = Validator::make($Request->all(), $Validation);
+
+        return $Validator->fails();
+    }
+
+    protected abstract function NotFound();
+}
