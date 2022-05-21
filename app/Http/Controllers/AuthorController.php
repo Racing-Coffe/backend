f55d30b49ebf7e1510e754279734765cf82adf17
DinosDev <erick@bilhalba.com.br>
2022-05-20 23:49:02 -0300

    fix(RC-03 02): Fix AuthorController Show Posts

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -35,12 +35,13 @@ class AuthorController extends Controller
     {
         $HiddenValues = ['content', 'category_id', 'author_id', 'created_at', 'updated_at'];
 
-        $Posts = Author::find($Request->id)
-            ->posts()
-            ->getResults()
-            ->makeHidden($HiddenValues);
+        $Author = Author::find($Request->id);
 
-        $PostsArray = $Posts->toArray();
+        if (!$Author) return response(['Error' => 'Author not found'], 404);
+
+        $Posts = $Author->posts()->getResults();
+
+        $PostsArray = $Posts->makeHidden($HiddenValues)->toArray();
 
         return $PostsArray;
     }
