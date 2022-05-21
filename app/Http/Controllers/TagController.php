ff158abbfb19188d261f4ddb4e55a9222c2089f6
DinosDev <erick@bilhalba.com.br>
2022-05-21 16:49:33 -0300

    feat(RC 05-02): Create Index Route for Tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends MainController
 {
     public function index(Request $Request)
     {
-        return "index";
+        $HiddenValues = ['description', 'created_at', 'updated_at'];
+
+        $Tags = Tag::all()->makeHidden($HiddenValues);
+
+        $TagsArray = $Tags->toArray();
+
+        return $TagsArray;
     }
 
     public function show(Request $Request)
