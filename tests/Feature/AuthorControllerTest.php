6eb9871954360b66ac3d682ceffb2530939d6f43
DinosDev <erick@bilhalba.com.br>
2022-05-28 01:47:31 -0300

    test(RC-11 00): Create Author Index Route Test

---
+++ tests/Feature/AuthorControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Author;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class AuthorControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed();
+    }
+
+    /**
+     * Test the Index Route of Author Controller.
+     *
+     * @return void
+     */
+    public function test_index_route()
+    {
+        $response = $this->get('/api/authors');
+
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+            $json->has(3)->first(
+                function (AssertableJson $json) {
+                    $json->hasAll(['id', 'name', 'description',]);
+                    $json->missingAll(['email', 'avatar', 'twitter', 'password', 'created_at', 'updated_at']);
+                }
+            )
+        );
+
+        $response->assertStatus(200);
+        $response->assertSuccessful();
+
+        $response->assertJsonCount(3);
+    }
+}
